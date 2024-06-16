2795280385df62454fb1a023fb502433a1834912
Yurij Boklan <hailrake@riseup.net>
2024-06-16 18:47:41 +0300

    add .gitignore

--- build/CMakeFiles/komaru-the-game.dir/src/main.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/komaru-the-game.dir/src/main.c.o: \
-  /home/hailrake/media/code/komarusoft/komaru-the-game-C/src/main.c \
-  /usr/include/raylib.h /usr/lib/clang/17/include/stdarg.h \
-  /usr/lib/clang/17/include/stdbool.h
