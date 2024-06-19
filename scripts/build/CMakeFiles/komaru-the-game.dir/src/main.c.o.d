ba534f92ad2c189c6d9bed7b9d78b4c4a8a0da79
Ttunay <tigrus232323k@gmail.com>
2024-06-19 19:22:57 +0300

    add about

---
+++ scripts/build/CMakeFiles/komaru-the-game.dir/src/main.c.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/komaru-the-game.dir/src/main.c.o: \
+  /home/tunay/komaru-the-game-C/src/main.c /usr/include/raylib.h \
+  /usr/lib/clang/17/include/stdarg.h /usr/lib/clang/17/include/stdbool.h \
+  /home/tunay/komaru-the-game-C/src/../include/about.h \
+  /home/tunay/komaru-the-game-C/src/../include/raygui.h
