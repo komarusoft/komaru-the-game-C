be3ef685dec959acd8f53c327cec1cb7a27f3c3c
Ttunay <tigrus232323k@gmail.com>
2024-06-30 12:38:13 +0300

    add raigui

--- src/about.c
+++ src/about.c
@@ -1,27 +1,16 @@
-// about.c
-
 #include "raylib.h"
-#include "../include/raylib.h"
-bool about_us(void)
+#include "../include/about.h"
+
+void about_us(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
 
-        DrawText("About Us", screenWidth / 2 - MeasureText("About Us", 40) / 2, screenHeight / 2 - 20, 40, DARKGRAY);
-        DrawText("Press [ESC] to return to menu", screenWidth / 2 - MeasureText("Press [ESC] to return to menu", 20) / 2, screenHeight - 60, 20, DARKGRAY);
+    DrawText("About Us", screenWidth / 2 - MeasureText("About Us", 40) / 2, screenHeight / 2 - 20, 40, DARKGRAY);
+    DrawText("Press [ESC] to return to menu", screenWidth / 2 - MeasureText("Press [ESC] to return to menu", 20) / 2, screenHeight - 60, 20, DARKGRAY);
 
-        EndDrawing();
-
-        if (IsKeyPressed(KEY_ESCAPE))
-            return true; 
-            
-            // Возвращаем true, если пользователь нажал ESC
-    }
-
-    return false; // Если окно закрыто, возвращаем false
+    EndDrawing();
 }
--- src/main.c
+++ src/main.c
@@ -1,83 +1,61 @@
 #include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "../include/raygui.h"
 #include "../include/about.h"
 
 typedef enum {
-  MENU_START,
-  MENU_ABOUT,
-  MENU_SETTINGS,
-  MENU_EXIT,
-  MENU_NONE
+    MENU_NONE,
+    MENU_START,
+    MENU_ABOUT,
+    MENU_SETTINGS,
+    MENU_EXIT
 } MenuOption;
 
 int main(void)
 {
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-  InitWindow(screenWidth, screenHeight, "raylib - меню");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib - меню");
 
-  SetTargetFPS(60);
+    SetTargetFPS(60);
 
-  MenuOption selectedOption = MENU_NONE;
-  MenuOption hoveredOption = MENU_NONE;
-
-  while (!WindowShouldClose())
-  {
-    Vector2 mousePoint = GetMousePosition();
-    hoveredOption = MENU_NONE;
-
-    if (CheckCollisionPointRec(mousePoint, (Rectangle){ screenWidth / 2 - 50, 120, 100, 40 })) {
-      hoveredOption = MENU_START;
-      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) selectedOption = MENU_START;
-    } else if (CheckCollisionPointRec(mousePoint, (Rectangle){ screenWidth / 2 - 50, 180, 100, 40 })) {
-      hoveredOption = MENU_ABOUT;
-      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) selectedOption = MENU_ABOUT;
-    } else if (CheckCollisionPointRec(mousePoint, (Rectangle){ screenWidth / 2 - 50, 240, 100, 40 })) {
-      hoveredOption = MENU_SETTINGS;
-      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) selectedOption = MENU_SETTINGS;
-    } else if (CheckCollisionPointRec(mousePoint, (Rectangle){ screenWidth / 2 - 50, 300, 100, 40 })) {
-      hoveredOption = MENU_EXIT;
-      if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) selectedOption = MENU_EXIT;
-    }
-
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    DrawText("Main Menu", screenWidth / 2 - MeasureText("Main Menu", 40) / 2, 40, 40, DARKGRAY);
-
-    DrawRectangle(screenWidth / 2 - 50, 120, 100, 40, LIGHTGRAY);
-    DrawRectangle(screenWidth / 2 - 50, 180, 100, 40, LIGHTGRAY);
-    DrawRectangle(screenWidth / 2 - 50, 240, 100, 40, LIGHTGRAY);
-    DrawRectangle(screenWidth / 2 - 50, 300, 100, 40, LIGHTGRAY);
-
-    DrawText("Start", screenWidth / 2 - MeasureText("Start", 20) / 2, 130, 20, hoveredOption == MENU_START ? RED : BLACK);
-    DrawText("About", screenWidth / 2 - MeasureText("About", 20) / 2, 190, 20, hoveredOption == MENU_ABOUT ? RED : BLACK);
-    DrawText("Settings", screenWidth / 2 - MeasureText("Settings", 20) / 2, 250, 20, hoveredOption == MENU_SETTINGS ? RED : BLACK);
-    DrawText("Exit", screenWidth / 2 - MeasureText("Exit", 20) / 2, 310, 20, hoveredOption == MENU_EXIT ? RED : BLACK);
-
-    EndDrawing();
-
-    // Handling menu options
-    switch (selectedOption)
+    MenuOption selectedOption = MENU_NONE;
+    
+    while (!WindowShouldClose())    
     {
-      case MENU_START:
-        // Actions for Start menu option
-        break;
-      case MENU_ABOUT:
-        about_us(); // Call about_us() and check its return value
-        selectedOption = MENU_NONE; // Reset selectedOption after returning from about_us()
-        main();
-        return 0;
-      case MENU_SETTINGS:
-        // Actions for Settings menu option
-        break;
-      case MENU_EXIT:
-        CloseWindow(); // Close window when Exit is selected
-        break;
-      default:
-        break;
-    }
-  }
+        if (selectedOption == MENU_NONE || selectedOption == MENU_START || selectedOption == MENU_SETTINGS || selectedOption == MENU_EXIT) {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
 
-  CloseWindow();
-  return 0;
+            DrawText("Main Menu", screenWidth / 2 - MeasureText("Main Menu", 40) / 2, 40, 40, DARKGRAY);
+
+            if (GuiButton((Rectangle){ screenWidth / 2 - 50, 120, 100, 40 }, "Start")) {
+                selectedOption = MENU_START;
+            }
+            if (GuiButton((Rectangle){ screenWidth / 2 - 50, 180, 100, 40 }, "About")) {
+                selectedOption = MENU_ABOUT;
+            }
+            if (GuiButton((Rectangle){ screenWidth / 2 - 50, 240, 100, 40 }, "Settings")) {
+                selectedOption = MENU_SETTINGS;
+            }
+            if (GuiButton((Rectangle){ screenWidth / 2 - 50, 300, 100, 40 }, "Exit")) {
+                selectedOption = MENU_EXIT;
+            }
+
+            EndDrawing();
+        } else if (selectedOption == MENU_ABOUT) {
+            about_us();
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                selectedOption = MENU_NONE;
+            }
+        }
+
+        if (selectedOption == MENU_EXIT) {
+            break;
+        }
+    }
+
+    CloseWindow();
+
+    return 0;
 }
